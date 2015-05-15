019ee7f6186d7f185124dc8b2afda28d48fef5bf
Benoît Laniel <nels+git@pgroupe.net>
2015-05-15 09:20:25 +0200

    Fix Wifi-Direct (inspired by p5100 CM device tree)

--- p52xx-common.mk
+++ p52xx-common.mk
@@ -60,6 +60,10 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
+
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0 \
     wifi.supplicant_scan_interval=15
