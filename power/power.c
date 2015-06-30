4bc669953a3ea28c34f4881e2be3189789ce2541
Benoît Laniel <nels+git@pgroupe.net>
2015-06-30 23:26:20 +0200

    Revert "Import Power HAL from android-ia (android-4.4.2_r1-ia2)"
    This reverts commit 380d85afefe61f4f76e9478e3226520f4fe06b86.

--- power/power.c
+++ power/power.c
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2014 Intel Corporation All Rights Reserved
  * Copyright (C) 2012 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,171 +14,156 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <errno.h>
-#include <string.h>
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include<time.h>
+#include <stdlib.h>
 
-#define LOG_TAG "Intel PowerHAL"
+//#define LOG_NDEBUG 0
+#define LOG_TAG "IntelPowerHAL"
 #include <utils/Log.h>
 
 #include <hardware/hardware.h>
 #include <hardware/power.h>
 
-#define TIMER_RATE_SYSFS	"/sys/devices/system/cpu/cpufreq/interactive/timer_rate"
-#define BOOST_PULSE_SYSFS	"/sys/devices/system/cpu/cpufreq/interactive/boostpulse"
-#define TOUCHBOOST_PULSE_SYSFS	"/sys/devices/system/cpu/cpufreq/interactive/touchboostpulse"
-#define TOUCHBOOST_SYSFS	"/sys/devices/system/cpu/cpufreq/interactive/touchboost_freq"
+#define BOOST_PULSE_SYSFS    "/sys/devices/system/cpu/cpufreq/interactive/boostpulse"
+#define BOOST_FREQ_SYSFS     "/sys/devices/system/cpu/cpufreq/interactive/hispeed_freq"
+#define BOOST_DURATION_SYSFS "/sys/devices/system/cpu/cpufreq/interactive/boostpulse_duration"
 
-/*
- * This parameter is to identify continuous touch/scroll events.
- * Any two touch hints received between a 20 interval ms is
- * considered as a scroll event.
- */
-#define SHORT_TOUCH_TIME 20
-
-/*
- * This parameter is to identify first touch events.
- * Any two touch hints received after 100 ms is considered as
- * a first touch event.
- */
-#define LONG_TOUCH_TIME 100
-
-/*
- * This parameter defines the number of vsync boost to be
- * done after the finger release event.
- */
-#define VSYNC_BOOST_COUNT 4
-
-/*
- * This parameter defines the time between a touch and a vsync
- * hint. the time if is > 30 ms, we do a vsync boost.
- */
-#define VSYNC_TOUCH_TIME 30
-
-struct intel_power_module{
-	struct power_module container;
-	int touchboost_disable;
-	int timer_set;
-	int vsync_boost;
+struct intel_power_module {
+    struct power_module container;
+    uint32_t pulse_duration;
+    struct timespec last_boost_time; /* latest POWER_HINT_INTERACTION boost */
 };
 
-static void sysfs_write(char *path, char *s)
+static ssize_t sysfs_write(char *path, char *s)
 {
     char buf[80];
-    int len;
+    ssize_t len;
     int fd = open(path, O_WRONLY);
 
     if (fd < 0) {
         strerror_r(errno, buf, sizeof(buf));
         ALOGE("Error opening %s: %s\n", path, buf);
-        return;
+        return -1;
     }
 
-    len = write(fd, s, strlen(s));
-    if (len < 0) {
+    if ((len = write(fd, s, strlen(s))) < 0) {
         strerror_r(errno, buf, sizeof(buf));
         ALOGE("Error writing to %s: %s\n", path, buf);
     }
 
     close(fd);
+    ALOGV("wrote '%s' to %s", s, path);
+
+    return len;
 }
-static int sysfs_read(char *path, char *s, int num_bytes)
+
+static ssize_t sysfs_read(char *path, char *s, int num_bytes)
 {
     char buf[80];
-    int count;
-    int ret = 0;
-	int fd = open(path, O_RDONLY);
-     if (fd < 0) {
+    ssize_t count;
+    int fd = open(path, O_RDONLY);
+
+    if (fd < 0) {
         strerror_r(errno, buf, sizeof(buf));
         ALOGE("Error reading from %s: %s\n", path, buf);
         return -1;
     }
+
     if ((count = read(fd, s, (num_bytes - 1))) < 0) {
         strerror_r(errno, buf, sizeof(buf));
         ALOGE("Error reading from  %s: %s\n", path, buf);
-        ret = -1;
     } else {
-        s[count] = '\0';
+        if ((count >= 1) && (s[count-1] == '\n')) {
+            s[count-1] = '\0';
+        } else {
+            s[count] = '\0';
+        }
     }
-	close(fd);
-    return ret;
+
+    close(fd);
+    ALOGV("read '%s' from %s", s, path);
+
+    return count;
 }
 
-static void intel_power_init(struct power_module *module)
+static void fugu_power_init(struct power_module *module)
 {
-	ALOGW("**Intel Power HAL initialisation**\n");
-	sysfs_write(TOUCHBOOST_SYSFS, INTEL_TOUCHBOOST_FREQ);
+    struct intel_power_module *mod = (struct intel_power_module *) module;
+    char boost_freq[32];
+    char boostpulse_duration[32];
+
+    /* Keep default boost_freq for fugu => max freq */
+
+    if (sysfs_read(BOOST_FREQ_SYSFS, boost_freq, 32) < 0) {
+        strcpy(boost_freq, "?");
+    }
+    if (sysfs_read(BOOST_DURATION_SYSFS, boostpulse_duration, 32) < 0) {
+        /* above should not fail but just in case it does use an arbitrary 20ms value */
+        snprintf(boostpulse_duration, 32, "%d", 20000);
+    }
+    mod->pulse_duration = atoi(boostpulse_duration);
+    /* initialize last_boost_time */
+    clock_gettime(CLOCK_MONOTONIC, &mod->last_boost_time);
+
+    ALOGI("init done: will boost CPU to %skHz for %dus on input events",
+            boost_freq, mod->pulse_duration);
 }
 
-static void intel_power_set_interactive(struct power_module *module, int on)
+static void fugu_power_set_interactive(struct power_module *module, int on)
 {
+    struct dirent **device_list = NULL;
+
+    ALOGI("setInteractive: on=%d", on);
+    (void) module; /* unused */
+    (void) on; /* unused */
 }
 
-static void intel_power_hint(struct power_module *module, power_hint_t hint,
-                       void *data) {
-	struct intel_power_module *intel = (struct intel_power_module *) module;
-	char sysfs_val[80];
-	static clock_t curr_time, prev_time , vsync_time;
-	float diff;
-	static int vsync_count;
-	static int consecutive_touch_int;
+static inline void timespec_sub(struct timespec *res, struct timespec *a, struct timespec *b)
+{
+    res->tv_sec = a->tv_sec - b->tv_sec;
+    if (a->tv_sec >= b->tv_sec) {
+        res->tv_nsec = a->tv_nsec - b->tv_nsec;
+    } else {
+        res->tv_nsec = 1000000000 - b->tv_nsec + a->tv_nsec;
+        res->tv_sec--;
+    }
+}
 
-	switch (hint) {
-		case POWER_HINT_INTERACTION:
-		curr_time = clock();
-		diff = (((float)curr_time - (float)prev_time) / CLOCKS_PER_SEC ) * 1000;
-		prev_time = curr_time;
-		if(diff < SHORT_TOUCH_TIME)
-			consecutive_touch_int ++;
-		else if (diff > LONG_TOUCH_TIME) {
-			intel->vsync_boost = 0;
-			intel->timer_set = 0;
-			intel->touchboost_disable = 0;
-			vsync_count = 0;
-			consecutive_touch_int = 0;
-		}
-		/* Simple touch: timer rate need not be changed here */
-		if((diff < SHORT_TOUCH_TIME) && (intel->touchboost_disable == 0)
-											&& (consecutive_touch_int > 4))
-			intel->touchboost_disable = 1;
-		/*
-		 *Scrolling: timer rate reduced to increase sensitivity. No more touch
-		 *boost after this
-		*/
-		if((intel->touchboost_disable == 1) && (consecutive_touch_int > 15)
-												&& (intel->timer_set == 0)) {
-			intel->timer_set = 1;
-		}
-		if (!intel->touchboost_disable) {
-			sysfs_write(TOUCHBOOST_PULSE_SYSFS,"1");
-		}
-		break;
-		case POWER_HINT_VSYNC:
+static inline uint64_t timespec_to_us(struct timespec *t)
+{
+    return t->tv_sec * 1000000 + t->tv_nsec / 1000;
+}
 
-		if(intel->touchboost_disable == 1) {
-			vsync_time = clock();
-			diff = (((float)vsync_time - (float)curr_time) / CLOCKS_PER_SEC ) * 1000;
-			if(diff > VSYNC_TOUCH_TIME) {
-				intel->timer_set = 0;
-				intel->vsync_boost = 1;
-				intel->touchboost_disable = 0;
-				vsync_count = VSYNC_BOOST_COUNT;
-			}
-		}
-		if(intel->vsync_boost) {
-			if((data == 1) && (vsync_count > 0)) {
-				sysfs_write(TOUCHBOOST_PULSE_SYSFS,"1");
-				vsync_count -- ;
-				if(vsync_count == 0)
-					intel->vsync_boost = 0;
-			}
-		}
-		break;
-		default:
-		break;
+static void fugu_power_hint(struct power_module *module, power_hint_t hint, void *data)
+{
+    struct intel_power_module *mod = (struct intel_power_module *) module;
+    char sysfs_val[80];
+    struct timespec curr_time;
+    struct timespec diff_time;
+    uint64_t diff;
+
+    (void) data;
+
+    switch (hint) {
+        case POWER_HINT_INTERACTION:
+            clock_gettime(CLOCK_MONOTONIC, &curr_time);
+            timespec_sub(&diff_time, &curr_time, &mod->last_boost_time);
+            diff = timespec_to_us(&diff_time);
+
+            ALOGV("POWER_HINT_INTERACTION: diff=%llu", diff);
+
+            if (diff > mod->pulse_duration) {
+                sysfs_write(BOOST_PULSE_SYSFS, "1");
+                mod->last_boost_time = curr_time;
+            }
+            break;
+        case POWER_HINT_VSYNC:
+            break;
+        default:
+            break;
     }
 }
 
@@ -185,24 +171,19 @@ static struct hw_module_methods_t power_module_methods = {
     .open = NULL,
 };
 
-
-
 struct intel_power_module HAL_MODULE_INFO_SYM = {
     container:{
-			common: {
-			tag: HARDWARE_MODULE_TAG,
-			module_api_version: POWER_MODULE_API_VERSION_0_2,
-			hal_api_version: HARDWARE_HAL_API_VERSION,
-			id: POWER_HARDWARE_MODULE_ID,
-			name: "Intel Power HAL",
-			author: "Power Management Team",
-			methods: &power_module_methods,
-	},
-    init: intel_power_init,
-    setInteractive: intel_power_set_interactive,
-    powerHint: intel_power_hint,
+        common: {
+            tag: HARDWARE_MODULE_TAG,
+            module_api_version: POWER_MODULE_API_VERSION_0_2,
+            hal_api_version: HARDWARE_HAL_API_VERSION,
+            id: POWER_HARDWARE_MODULE_ID,
+            name: "Fugu Power HAL",
+            author: "Intel",
+            methods: &power_module_methods,
+        },
+        init: fugu_power_init,
+        setInteractive: fugu_power_set_interactive,
+        powerHint: fugu_power_hint,
     },
-	touchboost_disable: 0,
-	timer_set: 0,
-	vsync_boost: 0,
 };
